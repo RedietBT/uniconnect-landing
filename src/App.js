e9878f6e5ebdc4cb9bd0828fe5a32938d21d6c5a
Arsema Addis <arsemadd@gmail.com>
2024-10-10 02:17:00 +0300

    Set up React project with Tailwind CSS

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="bg-blue-500 text-white text-center p-8">
+      <h1 className="text-4xl font-bold">Welcome to UniConnect!</h1>
+      <p className="text-lg mt-4">Building academic connections, fostering innovation.</p>
+      <button className="mt-6 bg-white text-blue-500 font-semibold py-2 px-4 rounded-lg hover:bg-blue-100">
+        Get Started
+      </button>
+    </div>
+  );
+}
+
+export default App;
